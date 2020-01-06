66975c63843dbe8d3b026824afd5b8ac10408d63
Willian Leandro <leandrodasilvawillian@gmail.com.br>
2020-01-06 16:48:00 -0300

    Criando PageAdmin com TPL AdminLTE

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 
 
@@ -11,9 +12,19 @@ $app = new Slim();
 
 $app->config('debug', true);
 
+// ROUTE INDEX SITE
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+// ROUTE INDEX ADMIN
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Hcode;
+
+
+class PageAdmin extends Page {
+
+
+	public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+	{	
+		// CHAMANDO O MÉTODO CONSTRUTOR DA CLASSE MÃE
+		parent::__construct($opts, $tpl_dir);
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+?>
