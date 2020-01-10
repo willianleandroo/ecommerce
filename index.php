1442f27b20c867336ad850b7ae2364ce9635641e
Willian Leandro <leandrodasilvawillian@gmail.com.br>
2020-01-10 18:26:35 -0300

    Criação da Recuperação de Senha

--- index.php
+++ index.php
@@ -94,6 +94,7 @@ $app->get('/admin/users/create', function () {
 	$page->setTpl("users-create");
 });
 
+
 // ROUTE PARA EXCLUIR UM USUÁRIO
 $app->get("/admin/users/:iduser/delete", function ($iduser) {
 
@@ -163,6 +164,92 @@ $app->post("/admin/users/:iduser", function ($iduser) {
 	header("Location: /admin/users");
 
 });
+
+
+// ROUTE PARA DO FORM PARA REDEFINIR SENHA
+$app->get("/admin/forgot", function () {
+
+	$page = new PageAdmin([
+		"header"	=>	false,
+		"footer"	=>	false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+// ROUTE RECEBE E-MAIL PARA RECUPERAR A SENHA VIA post DO FORM
+$app->post("/admin/forgot", function () {
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+
+// ROTA DA PÁGINA DE EMIAL DE RECUPERAÇÃO DE SENHA ENVIADO
+$app->get("/admin/forgot/sent", function () {
+
+	$page = new PageAdmin([
+		"header"	=>	false,
+		"footer"	=>	false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+// ROUTE DO LINK DE REDEFINIÇÃO DE SENHA ENVIADA POR EMAIL COM O PARAMETRO DO CÓDIGO ENVIADO
+$app->get("/admin/forgot/reset", function () {
+
+	//PEGANDO O CÓDIGO QUE ESTÁ NA URL DO LINK ENVIADO POR EMAIL E CARREGANDO DADOS DO USUÁRIO CORRESPONDENTE AO CÓDIGO
+	$user = User::validForgotDecrypt($_GET['code']);
+
+	$page = new PageAdmin([
+		"header"	=>	false,
+		"footer"	=>	false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"	=>	$user['desperson'],
+		"code"	=>	$_GET['code']
+	));
+
+});
+
+//ROUTE QUE REALIZA A TROCA DE SENHA, Q FOI PREENCHIDA NO FORM DE TROCAR A SENHA
+$app->post("/admin/forgot/reset", function () {
+
+	//PEGANDO O CÓDIGO QUE ESTÁ NO FORM DE REDEFINIR SENHA VIA POST E HIDDEN E CARREGANDO DADOS DO USUÁRIO CORRESPONDENTE AO CÓDIGO
+	$forgotData = User::validForgotDecrypt($_POST['code']);
+
+	User::setForgotUsed($forgotData['idrecovery']);
+
+	//CARREGANDO oOS DADOS DO USUÁRIO
+	$user = new User();
+
+	$user->get((int)$forgotData['iduser']);
+
+	// CRIPTROGRAFANDO A SENHA
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT, [
+		"cost"	=>	12
+	]);
+
+	// PASSANDO A SENHA RECEBIDA NO FORM POR PARAMETRO PARA A FUNÇÃO DE SETAR PASSWORD
+	$user->setPassword($password);
+
+
+	$page = new PageAdmin([
+		"header"	=>	false,
+		"footer"	=>	false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+});
+
 
 
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,12 +4,17 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 
 
 class User extends Model{
 
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_Secret";
+	const SECRET_IV = "HcodePhp7_Secret_IV";
+
+
 
 	public static function login ($login, $password)
 	{
@@ -142,6 +147,147 @@ class User extends Model{
 		));
 	}
 
+
+	// REDIFNIR SENHA RECEBENDO O E-MAIL DO FROM
+	public static function getForgot($email)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT *
+			FROM tb_persons a
+			INNER JOIN tb_users b USING(idperson)
+			WHERE a.desemail = :EMAIL ", array(
+				":EMAIL" => $email
+			));
+
+
+		if(count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha");
+			
+		}
+		else
+		{
+			$data = $results[0];
+		
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+				":iduser"	=>	$data['iduser'],
+				":desip"	=>	$_SERVER['REMOTE_ADDR']
+			));
+
+			if(count($results2) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha");
+			}
+			else
+			{
+
+				$dataRecovery = $results2[0];
+
+				// GERANDO UM CÓDIGO CRIPTOGRAFADO QUE SERÁ ENVIADO EM UM LINK POR EMAIL COM openssl_encrypt
+				//openssl_encrypt(data, method, password)
+				$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				// LINK QUE SERÁ ENVIADO POR EMAIL
+				$link = "http://www.willecommerce.com.br/admin/forgot/reset?code=$code";
+				/*
+				if ($data['inadmin'] === true) {
+					$link = "http://www.willecommerce.com.br/admin/forgot/reset?code=$code";
+				} else {
+					$link = "http://www.willecommerce.com.br/forgot/reset?code=$code";
+					
+				}*/				
+
+				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Will Ecommerce", "forgot", array(
+					"name"	=> $data['desperson'],
+					"link"	=>	$link
+				));
+
+				$mailer->send();
+
+				
+				return $link;
+
+
+
+			}
+
+
+		}
+
+
+	}
+
+
+	public static function validForgotDecrypt($code)
+	{
+
+		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT *
+			FROM tb_userspasswordsrecoveries a 
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+			", 
+			array(
+				":idrecovery"	=>	$idrecovery
+			));
+
+		if(count($results) === 0)
+		{
+			throw new Exception("Não foi possível recuperar a senha");
+			
+		}
+		else
+		{
+
+			return $results[0];
+		}
+
+
+	}
+
+	// MÉTODO QUE SETA DATA Q O CÓDIGO DE REDEFINIR SENHA FOI USADO 
+	public static function setForgotUsed($idrecovery)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"	=>	$idrecovery
+		));
+
+	}
+
+
+	//FUNÇÃO QUE ATUALIZA A SENHA DO USUÁRIO
+	public function setPassword($password)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"	=>	$password,
+			":iduser"	=>	$this->getiduser()
+		));
+
+	}
 
 }
 
